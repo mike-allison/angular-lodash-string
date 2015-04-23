2086700e02d4b187f33d4daae8a1d4c6c888b7de
Mike Allison <mike@mikeallisononline.com>
2015-04-23 09:19:34 -0500

    update for string use in filter

--- angular-lodash-string.js
+++ angular-lodash-string.js
@@ -1,15 +1,11 @@
-//created from from https://github.com/epeli/underscore.string/issues/285 & https://github.com/tlvince/angular-underscore-string
-
 //Before the definition of the app's module, add 'lodash' as a model,
 // providing the injectable '_' service, to which we'll mixin 'underscore.string' functions
-angular.module('lodash',[]).factory('_', function ($window) {
+angular.module('lodash.underscore.string',[]).factory('_', function ($window) {
     //Mixin underscore.string into lodash
     $window._.mixin($window._.string.exports());
     return $window._;
-});
-
-//for karma
-angular.module('lodash',[]).factory('lodashService', function ($window) {
+}).factory('lodashService', function ($window) {
+    //for karma
     if($window.lodashService){
         $window._thirdParty = $window._thirdParty || {};
         $window._thirdParty.lodashService = $window.lodashService;
@@ -17,29 +13,29 @@ angular.module('lodash',[]).factory('lodashService', function ($window) {
     }
     var lodashService = $window._thirdParty.lodashService;
     return lodashService;
+}).filter('s', function() {
+    /**
+     * Underscore.string
+     *
+     * Proxy filter for calling Underscore.string functions.
+     *
+     * Examples:
+     *
+     *     {{ name | s: 'swapCase' }}
+     *     {{ separator | s: 'join': ['foo', 'bar'] }}
+     *
+     * @param {String} s String to filter
+     * @param {String} fn Underscore.string function to call
+     * @param {[params]} params Extra parameters to pass to Underscore.string
+     * @return {String} Filtered string
+     */
+    return function(str, fn, params) {
+        str = str || '';
+        params = params || [];
+        if (typeof params === 'string')
+            params = [params];
+        params.unshift(str);
+        res = fn ? _.str[fn].apply(this, params) : str;
+        return res;
+    };
 });
-
-/**
- * Underscore.string
- *
- * Proxy filter for calling Underscore.string functions.
- *
- * Examples:
- *
- *     {{ name | s: 'swapCase' }}
- *     {{ separator | s: 'join': ['foo', 'bar'] }}
- *
- * @param {String} s String to filter
- * @param {String} fn Underscore.string function to call
- * @param {[params]} params Extra parameters to pass to Underscore.string
- * @return {String} Filtered string
- */
-angular.module('underscore.string', [])
-    .filter('s', ['lodash', function(_) {
-        return function(str, fn, params) {
-            str = str || '';
-            params = params || [];
-            params.unshift(str);
-            return fn ? _.str[fn].apply(this, params) : str;
-        };
-    }]);
